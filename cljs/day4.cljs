340ee4ca18106c636baadb8227860226e959669f
Ray McDermott <ray@mcdermott.be>
2022-12-10 18:25:51 +0100

    Explainer corrected

--- cljs/day4.cljs
+++ cljs/day4.cljs
@@ -98,15 +98,19 @@
 
 (defn explainer
   [inputs]
-  (let [input (->> (map parse inputs)
-                   (keep #(when (partially? %) %))
-                   (rand-nth))]
+  (let [[idx input] (->> inputs
+                         (map-indexed (fn [i v]
+                                        [i (parse v)]))
+                         (keep (fn [[i v]]
+                                 (when (fully? v)
+                                   [i v])))
+                         (rand-nth))
+        raw-input (nth inputs idx)]
     [:div#carouselExampleDark.carousel.carousel-dark.slide {:data-bs-ride "carousel"}
      [:div.carousel-indicators
       [:button.active {:type "button" :data-bs-target "#carouselExampleDark" :data-bs-slide-to "0" :class "" :aria-label "Slide 1"}]
       [:button {:type "button" :data-bs-target "#carouselExampleDark" :data-bs-slide-to "1" :aria-label "Slide 2" :class ""}]
-      [:button {:type "button" :data-bs-target "#carouselExampleDark" :data-bs-slide-to "2" :aria-label "Slide 3" :aria-current "true"}]
-      [:button {:type "button" :data-bs-target "#carouselExampleDark" :data-bs-slide-to "3" :aria-label "Slide 4" :aria-current "true"}]]
+      [:button {:type "button" :data-bs-target "#carouselExampleDark" :data-bs-slide-to "2" :aria-label "Slide 3" :aria-current "true"}]]
      [:div.carousel-inner
       [:div.carousel-item.active {:data-bs-interval "5000"}
        [:svg.bd-placeholder-img.bd-placeholder-img-lg.d-block.w-100
@@ -116,7 +120,7 @@
         [:text {:x "50%" :y "30%" :fill "#aaa" :dy ".3em"} (str (vec input))]]
        [:div.carousel-caption.d-none.d-md-block
         [:h4 "Parse into an array the two ranges the elves are tidying"]
-        [:p input]]]
+        [:p raw-input]]]
 
       [:div.carousel-item {:data-bs-interval "5000"}
        [:svg.bd-placeholder-img.bd-placeholder-img-lg.d-block.w-100
@@ -125,7 +129,7 @@
         [:text {:x "50%" :y "30%" :fill "#bbb" :dy ".3em"} (str (fully? input))]]
        (let [[A B X Y] (vec input)]
          [:div.carousel-caption.d-none.d-md-block
-          [:h5 (str "Let elements be [A " A " B " B " X " X " Y " Y "].
+          [:h5 (str "Let elements be " [A B X Y] ".
           Overlap check: (<= " A " " X " " Y " " B
                     ") or (<= " X " " A " " B " " Y ")")]
           [:p (str (vec input))]])]
@@ -135,28 +139,15 @@
         {:width "100%" :height "250" :xmlns "http://www.w3.org/2000/svg" :role "img" :aria-label "Placeholder: Third slide" :preserveAspectRatio "xMidYMid slice" :focusable "false"}
         [:rect {:width "100%" :height "100%" :fill "#e5e5e5"}]
         (if @part-1-visible?
-          [:text {:x "50%" :y "30%" :fill "#999" :dy ".3em"} (count (filter true? (map fully? (map parse inputs))))]
-          [:text {:x "50%" :y "30%" :fill "#999" :dy ".3em"} (count (filter true? (map partially? (map parse inputs))))])]
+          [:text {:x "50%" :y "30%" :fill "#999" :dy ".3em"} (str "Of " (count inputs) " there are " (part-1 inputs) " fully overlapping")]
+          [:text {:x "50%" :y "30%" :fill "#999" :dy ".3em"} (str "Of " (count inputs) " there are " (part-2 inputs) " partially overlapping")])]
        (if @part-1-visible?
          [:div.carousel-caption.d-none.d-md-block
           [:h5 "Keep the ranges which fully overlap"]
           [:p (str (fully? input))]]
          [:div.carousel-caption.d-none.d-md-block
           [:h5 "Keep the ranges which partially overlap"]
-          [:p (str (partially? input))]])]
-
-      [:div.carousel-item {:data-bs-interval "5000"}
-       [:svg.bd-placeholder-img.bd-placeholder-img-lg.d-block.w-100
-        {:width "100%" :height "250" :xmlns "http://www.w3.org/2000/svg" :role "img" :aria-label "Placeholder: Fourth slide" :preserveAspectRatio "xMidYMid slice" :focusable "false"}
-        [:rect {:width "100%" :height "100%" :fill "#e5e5e5"}]
-        (if @part-1-visible?
-          [:text {:x "50%" :y "30%" :fill "#999" :dy ".3em"} (str "Sum up " (count (filter true? (map fully? inputs))) " inputs = " (part-1 inputs))]
-          [:text {:x "50%" :y "30%" :fill "#999" :dy ".3em"} (str "Sum up " (count (filter true? (map partially? inputs))) " inputs = " (part-2 inputs))])]
-       [:div.carousel-caption.d-none.d-md-block
-        [:h5 "Sum them up"]
-        (if @part-1-visible?
-          [:p (count (filter true? (map fully? inputs)))]
-          [:p (count (filter true? (map partially? inputs)))])]]]
+          [:p (str (partially? input))]])]]
      [:button.carousel-control-prev {:type "button" :data-bs-target "#carouselExampleDark" :data-bs-slide "prev"}
       [:span.carousel-control-prev-icon {:aria-hidden "true"}]
       [:span.visually-hidden "Previous"]]
